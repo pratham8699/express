dc95663cb3e6a512fa504a9dcff58a44836d3936
Pratham <pratham84540@gmail.com>
2023-06-24 16:32:05 +0530

    Controller and routes both are added but still need some clarity

--- route/index.js
+++ route/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const homecontroller = require('../controllers/home_controller');
+router.get('/',homecontroller.home);
 console.log('router loaded and you can go to this route and do some shit as always');
 module.exports = router;
