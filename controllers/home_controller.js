a119ffc8e2700b87175b17b73ce7ae7a69ef0933
Pratham <pratham84540@gmail.com>
2023-06-24 16:42:06 +0530

    added router named pratham

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,6 @@
 module.exports.home = function(req,res){
     res.end('<h1>hello controller </h1>');
 }
+module.exports.pratham = function(req,res){
+    res.end('<h3>hey you are under route named pratham</h3>');
+}
--- route/index.js
+++ route/index.js
@@ -2,5 +2,6 @@ const express = require('express');
 const router = express.Router();
 const homecontroller = require('../controllers/home_controller');
 router.get('/',homecontroller.home);
+router.get('/Pratham',homecontroller.pratham);
 console.log('router loaded and you can go to this route and do some shit as always');
 module.exports = router;
